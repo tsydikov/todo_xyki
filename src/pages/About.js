26ea67c8eab202de7176071765528354a76af185
Tsudikov Kirill <tsydikov@gmail.com>
2021-04-26 17:55:42 +0300

    Add project files

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+            <div className="container">
+                <h1 className="display-4">Реакт приложение, которое использует Хуки и анимацию</h1>
+                <p className="lead">
+                    Версия  приложения <strong>1.0.42</strong>
+                </p>
+            </div>
+        </div>
+)
